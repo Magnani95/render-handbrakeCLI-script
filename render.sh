78c16b2ce823254a39485a675a2b7b4a1926dcd3
Andrea Magnani <31585313+Magnani95@users.noreply.github.com>
2020-12-08 22:00:46 +0100

    Add Queue functionality

--- render.sh
+++ render.sh
@@ -4,13 +4,17 @@
 # - Queue for list of input-renders and for finished one.
 # - All file in a directory
 
+#BUG TO FIX
+# - QUIET need to be VERBOSE
+# - FORCE_MODE should be FORCE+INPUT_ACCEPT+WRITE mode [-F -I -W]
+
 ##FUN DEFINITION
 echo "Function definition..."
 function argument_parsing_F()
 {
 	for p in $@; do
 		if [[ $p =~ -F|--force ]]; then
-			 FORCE_MODE='TRUE'
+			FORCE_MODE='TRUE'
 		fi
 	done
 
@@ -72,6 +76,27 @@ function argument_parsing_F()
 			FORCE_MODE='TRUE'
 			shift;
 			;;
+			#--Queue Management
+			-Q | --queue)
+			if [[ -e "${CONFIG_DIR}/queue" ]]; then
+				echo "Queue file exist" && echo "---"
+				cat "${CONFIG_DIR}/queue"
+				exit 1
+			else
+				touch "${CONFIG_DIR}/queue"
+				echo "Queue file does not exist. It has been created."
+				exit 1
+			fi
+			shift;
+			;;
+			-QA | --queue-add)
+			QUEUE_MODE='ADD'
+			shift;
+			;;
+			-QX | --queue-execution)
+			QUEUE_MODE='RUN'
+			shift;
+			;;
 			#OTHER
 			*)
 			echo "Param. '$1'" && echo "Exit the program (specify '-F|--force' otherwise)"
@@ -80,7 +105,7 @@ function argument_parsing_F()
 			;;
 		esac
 	done
-	if [[ -z $INPUT_PARAM && $FORCE_MODE =~ 'FALSE' ]]; then
+	if [[ -z ${INPUT_PARAM} && ${FORCE_MODE} =~ 'FALSE' && ! ${QUEUE_MODE} =~ RUN ]]; then
 		echo "Error, no Input file detected in parameters"
 		exit 2
 	fi
@@ -160,6 +185,38 @@ function getSub_F()
 	REPLY=''
 
 }
+
+function command_creation_F()
+{
+	FILEPATH=`realpath "$INPUT_PARAM"`
+
+	DIRPATH=`dirname "$FILEPATH"`
+	FILENAME=`echo "$FILEPATH"|sed 's|.*/\(.*\)\..*|\1|'`
+	echo "dir: "$DIRPATH""
+	echo "file: "$FILEPATH""
+
+	#--INPUT
+	INPUT_FILE="$FILEPATH"
+	#--OUTPUT FILE
+	OUTPUT_FILE="${OUTPUT_DIR}${FILENAME}.mkv"
+	if [[ -e $OUTPUT_FILE ]]; then
+		confirm_F
+	fi
+	#ENCODER SETTINGS
+	VIDEO="-e x265 -q $QUALITY --encoder-preset $PRESET"
+	AUDIO="--all-audio -E av_aac -6 dpl2 --all-subtitles "
+	FILTERS=''
+	EXTRA="-x pmode:pools='16'" #"wpp:pools='thread count'"		#var=val:var=val
+	#--SUB
+	getSub_F
+	if [[ -n $SUB_STRING ]]; then
+		SUB="--srt-file \""$SUB_STRING"\" "
+	fi
+	#--LANCIO RENDER
+	COMMAND="HandBrakeCLI -i \""${INPUT_FILE}"\" -o \""${OUTPUT_FILE}"\" $VIDEO $AUDIO $FILTERS $EXTRA "${SUB}" $QUIET"
+
+}
+
 ##-----------------------
 ##----------MAIN-------------
 echo "Variable definition..."
@@ -167,39 +224,46 @@ echo "Variable definition..."
 INPUT_PARAM=''
 QUALITY="23"
 PRESET="slow"
+REPLY=''
+#--Mode
 FIND_SUB='FALSE'
 FORCE_MODE='FALSE'
-OUTPUT_DIR="/hdd/Render/Output"
 QUIET="--verbose=0 2> /dev/null"
-REPLY=''
-
+QUEUE_MODE='FALSE'
+#--Directory
+OUTPUT_DIR="/hdd/Render/Output/"
+CONFIG_DIR='/hdd/Render/'
+#--
 argument_parsing_F "$@"
 
-FILEPATH=`realpath "$INPUT_PARAM"`
+if [[ ${QUEUE_MODE} = 'FALSE' ]]; then
+	command_creation_F "$@"
+	echo "++" && echo "$line" && echo "++"
+	eval "$COMMAND"
+	exit 0
+elif [[ ${QUEUE_MODE} = 'ADD' ]]; then
+	command_creation_F "$@"
+	echo "++" && echo "$line" && echo "++"
+	echo "$COMMAND" >> "${CONFIG_DIR}/queue"
+	echo "Job added to the queue"
+	exit 0
+elif [[ ${QUEUE_MODE} = 'RUN' ]]; then
+	QUEUE_LEN=`cat ${CONFIG_DIR}/queue | wc -l`
+	QUEUE_POS='1'
+	printf "Queue lenght:\t ${QUEUE_LEN}\n"
+	#RENDER=`head -n 1 ${CONFIG_DIR}/queue`
+	while IFS= read -r line; do
+		echo "--Running render ${QUEUE_POS} on ${QUEUE_LEN}..."
+		echo "++" && echo "$line" && echo "++"
+		eval "${line}"
+		tail -n +2 "${CONFIG_DIR}/queue" > "${CONFIG_DIR}/queue.tmp" && mv "${CONFIG_DIR}/queue.tmp" "${CONFIG_DIR}/queue"
+		echo "--Render ended" && QUEUE_POS=`expr $QUEUE_POS + 1`
+	done < "${CONFIG_DIR}/queue"
 
-DIRPATH=`dirname "$FILEPATH"`
-FILENAME=`echo "$FILEPATH"|sed 's|.*/\(.*\)\..*|\1|'`
-echo "dir: "$DIRPATH""
-echo "file: "$FILEPATH""
-
-#--INPUT
-INPUT_FILE="$FILEPATH"
-#--OUTPUT FILE
-OUTPUT_FILE="${OUTPUT_DIR}/${FILENAME}.mkv"
-if [[ -e $OUTPUT_FILE ]]; then
-	confirm_F
+	exit 0
+else
+	echo "DEF IMPOSSIBLE if-else in main-end "
+	exit 255
 fi
-#ENCODER SETTINGS
-VIDEO="-e x265 -q $QUALITY --encoder-preset $PRESET"
-AUDIO="--all-audio -E av_aac -6 dpl2 --all-subtitles "
-FILTERS=''
-EXTRA="-x pmode:pools=16" #"wpp:pools='thread count'"		#var=val:var=val
-#--SUB
-getSub_F
-if [[ -n $SUB_STRING ]]; then
-	SUB="--srt-file \""$SUB_STRING"\" "
-fi
-#--LANCIO RENDER
-COMMAND="HandBrakeCLI -i \""${INPUT_FILE}"\" -o \""${OUTPUT_FILE}"\" $VIDEO $AUDIO $FILTERS $EXTRA "${SUB}" $QUIET"
-echo "++" && echo "$COMMAND" && echo "++"
-eval "$COMMAND"
+echo "DEF IMPOSSIBLE in main-end "
+exit 255
